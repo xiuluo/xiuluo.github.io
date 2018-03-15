482123071b70ce648a08401d0adb78765dd61be0
xiuluo <chen919695074@gmail.com>
2018-03-15 22:47:07 +0800

    Site updated: 2018-03-15 22:47:06

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Feb 03 2018 20:31:39 GMT+0800 (CST)
+// build time:Thu Mar 15 2018 22:46:32 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
