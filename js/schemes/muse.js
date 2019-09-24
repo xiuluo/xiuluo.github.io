f8e6e9015c963d0421f7818dbcca162f86e2323a
xiuluo <chen919695074@gmail.com>
2019-09-24 21:42:52 +0800

    Site updated: 2019-09-24 21:42:51

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 22 2019 21:18:33 GMT+0800 (CST)
+// build time:Tue Sep 24 2019 21:42:48 GMT+0800 (CST)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
